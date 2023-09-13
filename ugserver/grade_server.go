d5dbf2f022d6042276f1995049c3790c196aa334
jeff_z <wxl.pure@gmail.com>
2023-09-14 01:03:10 +0800

    UserGradeServer 重命名

--- ugserver/grade_server.go
+++ ugserver/grade_server.go
@@ -7,22 +7,22 @@ import (
 	"user_growth/pb"
 )
 
-type UserGradeServer struct {
+type UgGradeServer struct {
 	pb.UnimplementedUserGradeServer
 }
 
-func (s *UserGradeServer) ListGrades(ctx context.Context, in *pb.ListGradesRequest) (*pb.ListGradesReply, error) {
+func (s *UgGradeServer) ListGrades(ctx context.Context, in *pb.ListGradesRequest) (*pb.ListGradesReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListGrades not implemented")
 }
-func (s *UserGradeServer) ListGradePrivileges(ctx context.Context, in *pb.ListGradePrivilegesRequest) (*pb.ListGradePrivilegesReply, error) {
+func (s *UgGradeServer) ListGradePrivileges(ctx context.Context, in *pb.ListGradePrivilegesRequest) (*pb.ListGradePrivilegesReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ListGradePrivileges not implemented")
 }
-func (s *UserGradeServer) CheckUserPrivilege(ctx context.Context, in *pb.CheckUserPrivilegeRequest) (*pb.CheckUserPrivilegeReply, error) {
+func (s *UgGradeServer) CheckUserPrivilege(ctx context.Context, in *pb.CheckUserPrivilegeRequest) (*pb.CheckUserPrivilegeReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CheckUserPrivilege not implemented")
 }
-func (s *UserGradeServer) UserGradeInfo(ctx context.Context, in *pb.UserGradeInfoRequest) (*pb.UserGradeInfoReply, error) {
+func (s *UgGradeServer) UserGradeInfo(ctx context.Context, in *pb.UserGradeInfoRequest) (*pb.UserGradeInfoReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UserGradeInfo not implemented")
 }
-func (s *UserGradeServer) UserGradeChange(ctx context.Context, in *pb.UserGradeChangeRequest) (*pb.UserGradeChangeReply, error) {
+func (s *UgGradeServer) UserGradeChange(ctx context.Context, in *pb.UserGradeChangeRequest) (*pb.UserGradeChangeReply, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UserGradeChange not implemented")
 }
