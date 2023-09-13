17179063798d562d9e1f0126e4b6971481e893b1
jeff_z <wxl.pure@gmail.com>
2023-09-14 01:04:21 +0800

    服务注册，验证服务

--- mainclient/main.go
+++ mainclient/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+	"user_growth/pb"
+)
 
+func main() {
+	// 连接到服务
+	addr := flag.String("addr", "localhost:80", "the address to connect to")
+	// 安全选项
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	}
+	conn, err := grpc.Dial(*addr, opts...)
+	if err != nil {
+		log.Fatalf("did not connect: error=%v\n", err)
+	}
+	defer conn.Close()
+	// 请求服务超时处理
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	// 新建客户端
+	cCoin := pb.NewUserCoinClient(conn)
+	cGrade := pb.NewUserGradeClient(conn)
+	// 测试1：UserCoinServer.ListTasks
+	r1, err1 := cCoin.ListTasks(ctx, &pb.ListTasksRequest{})
+	if err1 != nil {
+		log.Printf("cCoin.ListTasks error=%v\n", err1)
+	} else {
+		log.Printf("cCoin.ListTasks: %+v\n", r1.GetDatalist())
+	}
+	// 测试2：UserGradeServer.ListGrades
+	r2, err2 := cGrade.ListGrades(ctx, &pb.ListGradesRequest{})
+	if err2 != nil {
+		log.Printf("cGrade.ListGrades error=%v\n", err2)
+	} else {
+		log.Printf("cGrade.ListGrades: %+v\n", r2.GetDatalist())
+	}
+	// 测试3：修改积分
+	r3, err3 := cCoin.UserCoinChange(ctx, &pb.UserCoinChangeRequest{
+		Uid:  0,
+		Task: "abc",
+		Coin: 0,
+	})
+	if err3 != nil {
+		log.Printf("cCoin.UserCoinChange error=%v\n", err3)
+	} else {
+		log.Printf("cCoin.UserCoinChange: %+v\n", r3.GetUser())
+	}
 }
--- mainserver/main.go
+++ mainserver/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"user_growth/pb"
+	"user_growth/ugserver"
+)
 
+func main() {
+	lis, err := net.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	//creds, err := credentials.NewServerTLSFromFile("cert/server.pem", "cert/server.key")
+	//if err != nil {
+	//	log.Fatalf("credentials.NewServerTLSFromFile error=%v", err)
+	//}
+	//opts := []grpc.ServerOption{
+	//	grpc.WriteBufferSize(1024 * 1024 * 1), // 默认32KB
+	//	grpc.ReadBufferSize(1024 * 1024 * 1),  // 默认32KB
+	//	grpc.KeepaliveParams(keepalive.ServerParameters{
+	//		MaxConnectionIdle:     10 * time.Minute, // 没有消息的最长时间
+	//		MaxConnectionAge:      1 * time.Hour,    // 连接最长时间
+	//		MaxConnectionAgeGrace: 10 * time.Minute, // 最长时间后延迟关闭
+	//		Time:                  2 * time.Minute,  // ping间隔
+	//		Timeout:               3 * time.Second,  // ping超时
+	//	}),
+	//	grpc.MaxConcurrentStreams(1000),
+	//	grpc.ConnectionTimeout(time.Second * 1), // 连接超时
+	//	grpc.Creds(creds),
+	//}
+	//s := grpc.NewServer(opts...)
+	s := grpc.NewServer()
+	// 注册服务
+	pb.RegisterUserCoinServer(s, &ugserver.UgCoinServer{})
+	pb.RegisterUserGradeServer(s, &ugserver.UgGradeServer{})
+	reflection.Register(s)
+	// 启动服务
+	log.Printf("server listening at %v\n", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
