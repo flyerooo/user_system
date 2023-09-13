77c5d02a04d59b46c3380737309d8ca9db3137bb
jeff_z <wxl.pure@gmail.com>
2023-09-14 00:25:19 +0800

    实现user_growth_grpc.pb.go中UnimplementedUserGradeServer，UnimplementedUserGradeServer

--- ugserver/coin_server.go
+++ ugserver/coin_server.go
@@ -1,1 +1,25 @@
 package ugserver
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"user_growth/pb"
+)
+
+type UgCoinServer struct {
+	pb.UnimplementedUserCoinServer
+}
+
+func (s *UgCoinServer) ListTasks(ctx context.Context, in *pb.ListTasksRequest) (*pb.ListTasksReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListTasks not implemented")
+}
+func (s *UgCoinServer) UserCoinInfo(ctx context.Context, in *pb.UserCoinInfoRequest) (*pb.UserCoinInfoReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserCoinInfo not implemented")
+}
+func (s *UgCoinServer) UserDetails(ctx context.Context, in *pb.UserDetailsRequest) (*pb.UserDetailsReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserDetails not implemented")
+}
+func (s *UgCoinServer) UserCoinChange(ctx context.Context, in *pb.UserCoinChangeRequest) (*pb.UserCoinChangeReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserCoinChange not implemented")
+}
--- ugserver/grade_server.go
+++ ugserver/grade_server.go
@@ -1,1 +1,28 @@
 package ugserver
+
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"user_growth/pb"
+)
+
+type UserGradeServer struct {
+	pb.UnimplementedUserGradeServer
+}
+
+func (s *UserGradeServer) ListGrades(ctx context.Context, in *pb.ListGradesRequest) (*pb.ListGradesReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListGrades not implemented")
+}
+func (s *UserGradeServer) ListGradePrivileges(ctx context.Context, in *pb.ListGradePrivilegesRequest) (*pb.ListGradePrivilegesReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListGradePrivileges not implemented")
+}
+func (s *UserGradeServer) CheckUserPrivilege(ctx context.Context, in *pb.CheckUserPrivilegeRequest) (*pb.CheckUserPrivilegeReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CheckUserPrivilege not implemented")
+}
+func (s *UserGradeServer) UserGradeInfo(ctx context.Context, in *pb.UserGradeInfoRequest) (*pb.UserGradeInfoReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserGradeInfo not implemented")
+}
+func (s *UserGradeServer) UserGradeChange(ctx context.Context, in *pb.UserGradeChangeRequest) (*pb.UserGradeChangeReply, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UserGradeChange not implemented")
+}
